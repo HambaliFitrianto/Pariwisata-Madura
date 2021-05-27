c99ad89cae4ef1cc3cdec1067741122a411d28bb
alisaSugiarti <79351913+alisaSugiarti@users.noreply.github.com>
2021-05-27 21:08:05 +0700

    upload JS search Pamekasan

---
+++ js/searchPamekasan.js
@@ -0,0 +1,51 @@
+data = [
+    {
+        nama: "g1",
+        fasilitas: ["toilet", "ibadah", "penginapan"],
+    },
+    {
+        nama: "g2",
+        fasilitas: ["toilet", "resto", "penginapan"],
+    },
+    {
+        nama: "g3",
+        fasilitas: ["main"],
+    },
+    {
+        nama: "g4",
+        fasilitas: ["toilet", "parkir", "penginapan"],
+    },
+];
+
+function cari() {
+    var facilities = document.getElementsByName("facilities");
+    var key = [];
+    // ambil kata kuncinya
+    for (var i = 0; i < facilities.length; i++) {
+        if (facilities[i].checked) {
+            key.push(facilities[i].value);
+        }
+    }
+    console.table(key);
+    // cari data
+    for (var i = 0; i < data.length; i++) {
+        found = false;
+        counter=0;
+        for (var j = 0; j < key.length; j++) {
+            for (var k = 0; k < data[i]["fasilitas"].length; k++) {
+                if (key[j] == data[i]["fasilitas"][k]) {
+                    found = true;
+                    if (found) {
+                        counter+=1;
+                    }  
+                }
+            }
+        }
+        // jika ketemu
+        if (counter==key.length) {
+            document.getElementById(data[i]["nama"]).style.display = "block";
+        } else {
+            document.getElementById(data[i]["nama"]).style.display = "none";
+        }
+    }
+}
