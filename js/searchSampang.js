0822e0bc3b1f3136055eee08c5a5ca1a56f86df7
alisaSugiarti <79351913+alisaSugiarti@users.noreply.github.com>
2021-05-27 21:08:44 +0700

    upload JS search Sampang

---
+++ js/searchSampang.js
@@ -0,0 +1,47 @@
+data = [
+    {
+        nama: "pantai_Nepa",
+        fasilitas: ["toilet", "ibadah", "parkir","resto"],
+    },
+    {
+        nama: "waduk_Nepa",
+        fasilitas: ["toilet", "resto", "parkir"],
+    },
+    {
+        nama: "airTerjun_Toroan",
+        fasilitas: ["resto","parkir"],
+    },
+    {
+        nama: "goa_Lebar",
+        fasilitas: ["toilet", "parkir", "resto","ibadah"],
+    },
+];
+
+function cari() {
+    var facilities = document.getElementsByName("facilities");
+    var key = [];
+    // ambil kata kuncinya
+    for (var i = 0; i < facilities.length; i++) {
+        if (facilities[i].checked) {
+            key.push(facilities[i].value);
+        }
+    }
+    console.table(key);
+    // cari data
+    for (var i = 0; i < data.length; i++) {
+        found = false;
+        for (var j = 0; j < key.length; j++) {
+            for (var k = 0; k < data[i]["fasilitas"].length; k++) {
+                if (key[j] == data[i]["fasilitas"][k]) {
+                    found = true;
+                }
+            }
+        }
+        // jika ketemu
+        if (found) {
+            document.getElementById(data[i]["nama"]).style.display = "block";
+        } else {
+            document.getElementById(data[i]["nama"]).style.display = "none";
+        }
+    }
+}
