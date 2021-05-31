7b4dfde1f6c5b8a3ab673b60db3a7f26b9d7a978
HambaliFitrianto <83997977+HambaliFitrianto@users.noreply.github.com>
2021-06-01 03:32:50 +0700

    updata search pemekasan

--- js/searchPamekasan.js
+++ js/searchPamekasan.js
@@ -1,21 +1,45 @@
 data = [
     {
-        nama: "g1",
+        nama: "api",
         fasilitas: ["toilet", "ibadah", "penginapan"],
     },
     {
-        nama: "g2",
+        nama: "Goa_Blaban",
         fasilitas: ["toilet", "resto", "penginapan"],
     },
     {
-        nama: "g3",
+        nama: "pantai_Jumiang",
         fasilitas: ["main"],
     },
     {
-        nama: "g4",
+        nama: "bukit_Cinta",
         fasilitas: ["toilet", "parkir", "penginapan"],
     },
-];
+    {
+        nama: 'pantai_talang_siring',
+        fasilitas: ['resto', 'parkir', 'main'],
+    },
+    {
+        nama: 'bukit_brukoh',
+        fasilitas: ['resto', 'parkir'],
+    },
+    {
+        nama: 'pumcak_ratu',
+        fasilitas: ['resto', 'parkir', 'main'],
+    },
+    {
+        nama: 'pantai_mangrove_lembung',
+        fasilitas: ['resto', 'parkir'],
+    },
+    {
+        nama: 'vihara_avalokitesvara',
+        fasilitas: ['toilet', 'parkir', 'ibadah'],
+    },
+    {
+        nama: 'pantai_batu_kerbuy',
+        fasilitas: ['resto', 'parkir'],
+    },
+  ];
 
 function cari() {
     var facilities = document.getElementsByName("facilities");
